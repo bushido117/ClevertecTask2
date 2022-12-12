1fe5a35ade326150f83c08c4fc5d02c2f2f813a9
bushido117 <vadim.saiko53370@mail.ru>
2022-12-12 12:17:24 +0300

    Initial project setup

---
+++ Task2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task2
+//
+//  Created by Вадим Сайко on 12.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
